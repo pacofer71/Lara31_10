a072fb0479545f57cdaa0d1fdcfdcdd05e8d4887
Francisco Fernandez <pacofer71@gmail.com>
2019-10-31 17:25:05 +0100

    Migracion hecha

---
+++ database/migrations/2019_10_31_161653_create_libros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->text('sinopsis');
+            $table->string('portada')->nullable(true);
+            $table->integer('valoracion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
